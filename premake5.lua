f9d177e6b4a17950dac017006ec31daede9eb318
Vilska <106695564+Vilska@users.noreply.github.com>
2023-05-23 20:10:09 +0300

    Setup.

---
+++ premake5.lua
@@ -0,0 +1,20 @@
+project "SFML-ImGui-Dock"
+    kind "StaticLib"
+    language "C++"
+    
+    targetdir("bin/%{cfg.buildcfg}")
+    objdir("bin-int/%{cfg.buildcfg}")
+
+    files
+	{
+        "*.cpp",
+        "*.h"
+	}
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "on"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "on"
